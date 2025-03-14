1754cfc3fed876d648dde9386e66b597a27ba5da
aleasca99 <alexasca99@gmail.com>
2025-03-14 16:17:12 +0000

    Update routes/deploy.py file

--- backend/routes/deploy.py
+++ backend/routes/deploy.py
@@ -10,5 +10,5 @@ def deploy() -> Response:
   :return: Response object with a message and status code.
   """
   DeployNetwork.deploy_network()
-  DeployNetwork._internet_router_configuration
+  DeployNetwork.internet_router_configuration()
   return jsonify({}), 204
